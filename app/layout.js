0ea393d1ee1b1623fab71a1507959250131b306c
sxmdln <57649092+sxmdln@users.noreply.github.com>
2024-07-10 17:17:27 +0800

    Added favicon

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
       <body className={inter.className}>{children}</body>
     </html>
   );
